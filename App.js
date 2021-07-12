8834247e8e6364a204f04d612aa52e2487914c73
leannelb <leannelaceybyrne@outlook.com>
2021-07-12 20:26:19 +0100

    working app, styled, generates quotes

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, ImageBackground, Text, View, Image } from "react-native";
+import QuoteAndAuthor from "./components/QuoteAndAuthor";
 
 export default function App() {
+  const image = { uri: "asset:/images/peacfulImage.jpg" };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        style={styles.imgBackground}
+        resizeMode="cover"
+        source={require("./assets/images/peacfulImage.jpg")}
+      >
+        <View>
+          <QuoteAndAuthor />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -14,8 +24,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  imgBackground: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+  },
+  // container: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   backgroundColor: '#F5FCFF',
+  // },
+  headerText: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+    fontWeight: "bold",
   },
 });
---
+++ components/QuoteAndAuthor.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react";
+import { StyleSheet, FlatList, View, Modal, Text, Button } from "react-native";
+import axios from "axios";
+
+class QuoteAndAuthor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "",
+      author: "",
+    };
+  }
+
+  componentDidMount() {
+    this.getQuote();
+  }
+
+  getQuote() {
+    let url =
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
+    axios.get(url).then((res) => {
+      console.log(res);
+      let data = res.data.quotes;
+      let quoteNum = Math.floor(Math.random() * data.length); //quote number
+      let randomQuote = data[quoteNum]; //actual quote
+
+      this.setState({
+        quote: randomQuote["quote"],
+        author: randomQuote["author"],
+      });
+    });
+  }
+
+  getNewQuote = () => {
+    this.getQuote();
+  };
+
+  render() {
+    const { quote, author } = this.state;
+
+    return (
+      <View style={styles.screen}>
+        <View style={styles.container}>
+          <Text style={styles.mainTitle}>Daily Quote</Text>
+
+          <View style={styles.fontArea}>
+            <Text style={styles.quoteBox}>{quote}</Text>
+            <Text style={styles.author}>{author}</Text>
+          </View>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            color="#44014C"
+            style={styles.buttonStyle}
+            title="New Quote"
+            onPress={this.getNewQuote}
+          ></Button>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 30,
+    flexDirection: "column",
+  },
+  container: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1
+  },
+  mainTitle: {
+    color: "#ffffff",
+    fontWeight: "bold",
+    fontStyle: 'italic',
+    fontSize: 40,
+    fontFamily: "ZenTokyoZoo-Regular",
+    paddingVertical: 10,
+  },
+  fontArea: {
+    paddingTop: 20,
+  },
+  quoteBox: {
+    fontSize: 25,
+    fontFamily: "Roboto",
+    fontWeight: 400,
+    padding: 10,
+    marginTop: 20,
+    color: "#E9F1F7",
+    backgroundColor: "rgba(65, 26, 201, 0.5)",
+  },  
+  author: {
+    padding: 10,
+    marginTop: 20,
+    color: "#ffffff",
+    fontSize: 20,
+    backgroundColor: "rgba(65, 26, 201, 0.3)",
+    justifyContent: "flex-start",
+    alignItems: "baseline",
+    alignSelf: "baseline",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignContent: "flex-start"
+  },  
+  box: {
+    width: 50,
+    height: 50,
+  },
+  row: {
+  
+  },
+  buttonContainer: {
+    paddingTop: 50,
+  },
+  buttonStyle: {
+    paddingTop: 50,
+    margin: 50,
+  },
+
+});
+
+export default QuoteAndAuthor;
